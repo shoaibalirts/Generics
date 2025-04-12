4dd8c01f9e376a2796abee4a19ccac022b270300
shoaibalirts <shoaibali.rts@gmail.com>
2025-04-12 16:52:44 +0200

    generic class with constraint (want to accept Student or its child classes only)

---
+++ ClassLibrary1/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public abstract class Student
+    {
+        public abstract int Marks { get; set; }
+    }
+
+    public class GraduateStudent : Student
+    {
+        public override int Marks { get; set; }
+    }
+    public class PostGraduateStudent : Student
+    {
+        public override int Marks { get; set; }
+    }
+
+    // generic class with constraint (want to accept Student or its child classes only)
+    public class MarksPrinter<T> where T : Student
+    {
+        // stu is a field with generic type T
+        public T stu;
+        public void PrintMarks()
+        {
+            Student temp = (Student)stu; // type cast the stu field to Student 
+            Console.WriteLine(temp.Marks);
+        }
+    }
+}
--- Generics/Program.cs
+++ Generics/Program.cs
@@ -7,33 +7,39 @@ namespace Generics
     {
         public static void Main(string[] args)
         {
-            User<int> user1 = new User<int>();
-            User<bool> user2 = new User<bool>();
-            user1.RegistrationStatus = 1234;
-            user2.RegistrationStatus = true;
-            WriteLine(user1.RegistrationStatus);
-            WriteLine(user2.RegistrationStatus);
+            {
+                User<int> user1 = new User<int>();
+                User<bool> user2 = new User<bool>();
+                user1.RegistrationStatus = 1234;
+                user2.RegistrationStatus = true;
+                WriteLine(user1.RegistrationStatus);
+                WriteLine(user2.RegistrationStatus);
 
-            Human<int> std1 = new Human<int>();
-            Human<string> std2 = new Human<string>();
+                Human<int> std1 = new Human<int>();
+                Human<string> std2 = new Human<string>();
 
-            std1.Rank = 1;
-            std2.Rank = "Absent";
-            WriteLine(std1.Rank);
-            WriteLine(std2.Rank);
+                std1.Rank = 1;
+                std2.Rank = "Absent";
+                WriteLine(std1.Rank);
+                WriteLine(std2.Rank);
 
-            Person<int, int> person1 = new Person<int, int>();
-            Person<int, string> person2 = new Person<int, string>();
-            person1.PersonNumber = 1;
-            person1.Age = 25;
-            person2.PersonNumber = 2;
-            person2.Age = "30-35";
+                Person<int, int> person1 = new Person<int, int>();
+                Person<int, string> person2 = new Person<int, string>();
+                person1.PersonNumber = 1;
+                person1.Age = 25;
+                person2.PersonNumber = 2;
+                person2.Age = "30-35";
 
-            WriteLine($"person1 number: {person1.PersonNumber}");
-            WriteLine($"person1 age: {person1.Age}");
-            WriteLine($"person2 number: {person2.PersonNumber}");
-            WriteLine($"person2 age: {person2.Age}");
+                WriteLine($"person1 number: {person1.PersonNumber}");
+                WriteLine($"person1 age: {person1.Age}");
+                WriteLine($"person2 number: {person2.PersonNumber}");
+                WriteLine($"person2 age: {person2.Age}");
 
+            }
+
+            MarksPrinter<GraduateStudent> graduatestudent1 = new MarksPrinter<GraduateStudent>();
+            graduatestudent1.stu = new GraduateStudent() { Marks = 80 };
+            graduatestudent1.PrintMarks();
 
         }
     }
